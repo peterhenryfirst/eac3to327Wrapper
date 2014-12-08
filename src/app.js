9821ac424333c946e4281ef54b8ffa89e656e825
Peter Pereira <pereira.pedro.h@gmail.com>
2014-12-08 02:13:34 +0100

    First commit to test

---
+++ src/app.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var spawn = require('child_process').spawn,
+	exePath = path.dirname(__dirname),
+	pathBRD = 'E:\brdrip\Archer2\ARCHER_SEASON_2_DISC_1';
+
+var eac3toExe = spawn(exePath + '/eac3to', [], {
+	cwd: exePath
+});
+
+eac3toExe.stdout.on('data', function (data) {
+	console.log('stdout: ' + data);
+});
+
+eac3toExe.stderr.on('data', function (data) {
+	console.log('stderr: ' + data);
+});
+
+eac3toExe.on('close', function (code) {
+	console.log('child process exited with code ' + code);
+});
